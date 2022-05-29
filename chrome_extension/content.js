a7c724da8bba6c9da5565053782cca87ad1bf669
Piyush Ajmani <piyushajmani5@gmail.com>
2022-05-29 04:16:15 -0500

    Some cleanup
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -3,8 +3,10 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 const ENDPOINT = 'https://lit-dusk-60294.herokuapp.com/api/v1/change-shift'
 
 const makeChangeShiftRequest = () => {
+    // Extract date and time of the event
     const dateElement = document.getElementsByClassName('amqcKf')[0]
 
+    // Show Loading
     const spinner = document.getElementById("spinner");
     spinner.removeAttribute('hidden');
 
@@ -17,8 +19,7 @@ const makeChangeShiftRequest = () => {
             time: dateElement.nextElementSibling.textContent,
         })
     }).then(response => response.json())
-        .then(data => {
-            localStorage.setItem('replacement', data.name)
+        .then(() => {
             spinner.setAttribute('hidden', '')
             alert('Se envió el solicitud de cambio de turno')
             location.reload();
@@ -27,52 +28,61 @@ const makeChangeShiftRequest = () => {
         alert('Algo salió mal con la solicitud. Por favor, inténtelo de nuevo más tarde.')
     })
 
-
 }
 
-
-const insertChangeShiftOption = () => {
-    const eventTitleElement = document.getElementById('rAECCd')
+const insertSpinner = prevNode => {
     let spinner = document.getElementById('spinner')
-    if (spinner == null) {
-        spinner = document.createElement('div')
-        spinner.innerHTML = '<div hidden id="spinner">';
-        if (eventTitleElement) {
-            eventTitleElement.parentNode.insertBefore(spinner, eventTitleElement.nextSibling);
-        }
-    }
-    if (eventTitleElement) {
-        const eventTitle = eventTitleElement.innerText
-        if (eventTitle.startsWith('chat:')) {
-            const responseOptions = document.getElementsByClassName('YWILgc NAFvr')[0];
 
-            let changeOption = document.getElementById('changeOption');
-            if (changeOption == null) {
-                changeOption = document.createElement('div')
-                changeOption.innerHTML = '<div class="Vwe4Vb MbhUzd" jsname="ksKsZd"></div><div class="ZFr60d CeoRYc"></div><span jsslot="" class="CwaK9"><span class="RveJvd snByac"><span class="AclISc">Cambiar turno</span></span></span>';
-                changeOption.setAttribute('id', 'changeOption')
-                changeOption.setAttribute('role', 'button')
-                changeOption.setAttribute('class', 'U26fgb O0WRkf oG5Srb C0oVfc ZGVUP Ztzsdd KKjvXb M9Bg4d')
-                changeOption.setAttribute('tabindex', '0');
-                responseOptions.parentNode.insertBefore(changeOption, responseOptions.nextSibling);
-                changeOption.onclick = makeChangeShiftRequest;
-            }
-
-        }
+    // Spinner already exists in the document
+    if (spinner != null) {
+        return;
     }
 
+    spinner = document.createElement('div')
+    spinner.innerHTML = '<div hidden id="spinner">';
+    prevNode.parentNode.insertBefore(spinner, prevNode.nextSibling);
+}
+
+const insertChangeShiftButton = prevNode => {
+    let changeOption = document.getElementById('changeOption');
+
+    // Change option button already exists in the document
+    if (changeOption != null) {
+        return;
+    }
+
+    changeOption = document.createElement('div')
+    // classes taken from gCal
+    changeOption.innerHTML = '<div class="Vwe4Vb MbhUzd" ></div><div class="ZFr60d CeoRYc"></div><span class="CwaK9"><span class="RveJvd snByac"><span class="AclISc">Cambiar turno</span></span></span>';
+    changeOption.setAttribute('id', 'changeOption')
+    changeOption.setAttribute('role', 'button')
+    changeOption.setAttribute('class', 'U26fgb O0WRkf oG5Srb C0oVfc ZGVUP Ztzsdd KKjvXb M9Bg4d')
+    changeOption.setAttribute('tabindex', '0');
+    changeOption.onclick = makeChangeShiftRequest;
+
+    prevNode.parentNode.insertBefore(changeOption, prevNode.nextSibling);
+}
+
+const execute = () => {
+    const eventTitleElement = document.getElementById('rAECCd')
+
+    // Event details modal is closed or the event is not a support chat event
+    if (eventTitleElement == null || !eventTitleElement.innerText.startsWith('chat:')) {
+        return;
+    }
+
+    insertSpinner(eventTitleElement);
+
+    const responseOptions = document.getElementsByClassName('YWILgc NAFvr')[0];
+    insertChangeShiftButton(responseOptions);
 };
 
 const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
-        insertChangeShiftOption(mutation.target);
+        execute(mutation.target);
     });
 });
 
 observer.observe(document, {
     subtree: true, childList: true,
 });
-
-
-
-
